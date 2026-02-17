158141e14ba31739b1518bb340c055ae579f6d1b
muhammadkhaleel0314-byte <muhammadkhaleel0314@gmail.com>
2026-02-17 16:41:53 +0500

    Update recovery_app.py
    Signed-off-by: muhammadkhaleel0314-byte <muhammadkhaleel0314@gmail.com>

--- recovery_app.py
+++ recovery_app.py
@@ -77,7 +77,6 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
-st.set_page_config(page_title="Sustainability Report", layout="wide")
 st.subheader("Sustainability Report - مکمل ٹول")
 
 # ---------------- SIDEBAR ---------------- #
@@ -1334,6 +1333,7 @@ st.download_button(
     file_name="recovery_summary.pdf",
     mime="application/pdf"
 )
+
 
 
 

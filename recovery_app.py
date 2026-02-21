17057fa354316edd680c91dac3963109116abbc6
muhammadkhaleel0314-byte <muhammadkhaleel0314@gmail.com>
2026-02-21 07:04:36 +0500

    Update recovery_app.py
    Signed-off-by: muhammadkhaleel0314-byte <muhammadkhaleel0314@gmail.com>

--- recovery_app.py
+++ recovery_app.py
@@ -260,7 +260,6 @@ if not df_display.empty:
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(layout="wide")
 
 st.title("MDP Calculator - Active Loan Based")
 
@@ -1412,6 +1411,7 @@ if files:
         file_name="merged_data.csv",
         mime="text/csv"
     )
+
 
 
 

88511a7b232e01f1df17bc2e91bb84f2a8cf0277
muhammadkhaleel0314-byte <muhammadkhaleel0314@gmail.com>
2026-02-23 09:29:43 +0500

    Update recovery_app.py
    Signed-off-by: muhammadkhaleel0314-byte <muhammadkhaleel0314@gmail.com>

--- recovery_app.py
+++ recovery_app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Login System",)
 # ---------- USERS ----------
 USERS = {
     "Khaleel": "112345",
-    "Baqir@0315": "Baq0ir315",
+    "Baqir@0315": "Baqir315",
     "SaneelHaider": "Haider222",
     "Atif Sajjad": "Sajjad222",
     
@@ -1292,6 +1292,7 @@ st.download_button(
     file_name="recovery_summary.pdf",
     mime="application/pdf"
 )
+
 
 
 

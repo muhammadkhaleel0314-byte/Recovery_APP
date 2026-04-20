a10a537b8bab3a30a2b3e4f5211cde677ee4392c
muhammadkhaleel0314-byte <muhammadkhaleel0314@gmail.com>
2026-04-20 11:54:10 +0500

    Update recovery_app.py
    Signed-off-by: muhammadkhaleel0314-byte <muhammadkhaleel0314@gmail.com>

--- recovery_app.py
+++ recovery_app.py
@@ -47,9 +47,11 @@ a, a:visited {
 link1 = "https://script.google.com/macros/s/AKfycbyCr-KxEdrWdLhHXYWMLcDdK1Viciar6yJtQsaQNPFuY-X9IK41GkOiRvkV64PbatE9Kg/exec"
 link2 = "https://script.google.com/macros/s/AKfycbzPaNIURbCJLA8iNRm3RA6v2OS5WlxaMURw0IIjcwLnLvBPlVJ9DJG7KxihITJ6g6mb/exec"
 link3 = "https://script.google.com/macros/s/AKfycbzTMdTGQczpfnWd76rdwhj1rr_4DCMTMITYFgJZBcGrDbBSQZIHKu3mTkaXYnB5Y9VZew/exec"
+link4 = "https://pmybals.pmyp.gov.pk/"
+link5 = "https://acag.punjab.gov.pk/"
 
 # ----------- BOXES -----------
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, co14, col5 = st.columns(3)
 
 with col1:
     st.markdown(f'''
@@ -69,5 +71,17 @@ with col3:
     st.markdown(f'''
         <a href="{link3}" target="_blank">
             <div class="box box3">ACAG All Batch's Data</div>
+        </a>
+    ''', unsafe_allow_html=True)
+with col4:
+    st.markdown(f'''
+        <a href="{link3}" target="_blank">
+            <div class="box box3">PMY Apply And Track Application</div>
+        </a>
+    ''', unsafe_allow_html=True)
+    with col5:
+    st.markdown(f'''
+        <a href="{link3}" target="_blank">
+            <div class="box box3">ACAG Apply</div>
         </a>
     ''', unsafe_allow_html=True)
